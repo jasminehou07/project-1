b9643c77a03e37af47ec3019d5bd8aee0916b8a2
Jasmine Hou <jasminehou._@berkeley.edu>
2025-09-23 18:01:45 -0700

    Adding interactive graph

--- project.R
+++ project.R
@@ -22,3 +22,34 @@ g <- graph_from_adjacency_matrix(
 #plot the igraph
 plot(g)
 
+# adding monks to the graph
+n <- nrow(monk_mat)
+
+nodes <- data.frame(
+  id    = 0:(n - 1),
+  name  = colnames(monk_mat),
+  group = 1L
+)
+
+# makes a table of all of the monk connections
+edge_idx <- which(monk_mat > 0, arr.ind = TRUE)
+links <- data.frame(
+  source = edge_idx[, 1] - 1,        # 0-indexed
+  target = edge_idx[, 2] - 1,        # 0-indexed
+  value  = monk_mat[edge_idx]        # tie strength
+)
+
+# save the interactive graph to fn
+fn <- forceNetwork(
+  Links = links, Nodes = nodes,
+  Source = "source", Target = "target",
+  NodeID = "name", Group = "group", 
+  Value = "value",
+  fontSize = 14, opacity = 0.85, zoom = TRUE, charge = -180
+)
+
+#show the interactive graph
+fn
+
+
+
